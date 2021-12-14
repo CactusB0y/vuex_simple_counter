8d6fe2962046ef812f29954eea263e958e078191
CactusB0y <68423704+CactusB0y@users.noreply.github.com>
2021-12-14 01:36:45 +0100

    finish

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h1>Voici mon compteur</h1>
+    <p>{{count}}</p>
+    <Counter/>
+  </div>
+</template>
+
+<script>
+import index from './store/index.js'
+import Counter from './components/Counter.vue'
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      index: index
+    }
+  },
+  components:{
+    Counter
+  },
+  computed:{
+    count(){
+      return index.state.count
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    <h1>Voici mes boutons</h1>
+    <button @click='increment()'>+</button>
+    <button @click='decrement()'>-</button>
+</div>
+</template>
+
+<script>
+import index from '../store/index.js'
+
+export default {
+    name: 'Counter',
+    methods: {
+        increment() {
+            index.commit('increment')
+        },
+        decrement() {
+            index.commit('decrement')
+        },
+    },
+}
+</script>
+
+<style>
+
+</style>
